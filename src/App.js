4ba69bfd9c8390776b8525b42b95554e75c28b2d
Nguyen Minh Hieu <86197383+HieuNguyenDev@users.noreply.github.com>
2022-09-25 20:49:05 +0700

    Bài 94. Cấu hình sử dụng CSS/SASS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import Button from "~/components/Button";
-
 function App() {
-  return (
-    <div className="App">
-      <Button />
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>React App</h1>
+        </div>
+    );
 }
 
 export default App;
